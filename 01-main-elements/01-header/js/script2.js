50c5717141abfe887aab354d264d7843fdbf63f3
yaroprof <crosskyiv@gmail.com>
2024-09-08 16:36:31 +0300

    final02-com

--- 01-main-elements/01-header/js/script2.js
+++ 01-main-elements/01-header/js/script2.js
@@ -4,7 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     menuToggle.addEventListener('click', () => {
         sidebar.classList.toggle('fullscreen');
-        menuToggle.classList.toggle('active'); // Додаємо або прибираємо клас 'active'
+        if (sidebar.classList.contains('fullscreen')) {
+            menuToggle.style.right = '20px';
+            menuToggle.style.top = '20px';
+        } else {
+            menuToggle.style.right = 'initial';
+            menuToggle.style.left = '50%';
+            menuToggle.style.transform = 'translateX(-50%)';
+        }
     });
-    
 });
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuToggle = document.querySelector('menu-toggle');
+//     const sidebar = document.querySelector('sidebar');
+
+//     menuToggle.addEventListener('click', () => {
+//         sidebar.classList.toggle('fullscreen');
+//         // menuToggle.classList.toggle('active'); // Додаємо або прибираємо клас 'active'
+//     });
+    
+// });
