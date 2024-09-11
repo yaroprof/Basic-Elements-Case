8bdcca8b991c99aed6ba4e56d0fdb0e11dc20693
yaroprof <crosskyiv@gmail.com>
2024-09-11 19:18:40 +0300

    nnm

---
+++ 01-main-elements/03-features/js/script1.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelectorAll('.title').forEach(title => {
+//     title.addEventListener('click', () => {
+//         document.querySelectorAll('.title').forEach(t => {
+//             if (t !== title) t.classList.remove('title-expanded');
+//         });
+//         title.classList.toggle('title-expanded');
+//     });
+// });
+
+// document.querySelectorAll('.more-button').forEach(button => {
+//     button.addEventListener('click', () => {
+//         const content = button.nextElementSibling;
+//         if (content.classList.contains('hidden')) {
+//             content.classList.remove('hidden');
+//             button.textContent = 'Less';
+//         } else {
+//             content.classList.add('hidden');
+//             button.textContent = 'More';
+//         }
+//     });
+// });
